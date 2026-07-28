269bd46515e06f52f30c78380421f4548244f07a
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2026-07-28 18:53:20 -0300

    custom cursor

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,41 +1,51 @@
 import type { Metadata } from 'next'
 import localFont from 'next/font/local'
 
+import CustomCursor from '@/components/CustomCursor'
+
 import './globals.css'
 
 const franklinGothic = localFont({
-  src: './fonts/FranklinGothic.ttf',
-  variable: '--font-franklin-gothic',
-  display: 'swap',
+    src: './fonts/FranklinGothic.ttf',
+    variable: '--font-franklin-gothic',
+    display: 'swap',
 })
 
 export const metadata: Metadata = {
-  title: 'Mafalda Produções',
-  description: '',
+    title: 'Mafalda Produções',
+    description: '',
 }
 
+type RootLayoutProps = Readonly<{
+    children: React.ReactNode
+}>
+
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode
-}>) {
-  return (
-    <html
-      lang="pt-BR"
-      className={`${franklinGothic.variable} h-full antialiased`}
-    >
-      <body
-        className={`
-          flex
-          min-h-screen
-          flex-col
-          bg-black
-          text-[#F4EEDB]
-          [font-family:monospace]
-        `}
-      >
-        {children}
-      </body>
-    </html>
-  )
+    children,
+}: RootLayoutProps) {
+    return (
+        <html
+            lang="pt-BR"
+            className={`
+                ${franklinGothic.variable}
+                h-full
+                antialiased
+            `}
+        >
+            <body
+                className="
+                    flex
+                    min-h-screen
+                    flex-col
+                    bg-black
+                    text-[#F4EEDB]
+                    [font-family:monospace]
+                "
+            >
+                {children}
+
+                <CustomCursor />
+            </body>
+        </html>
+    )
 }
