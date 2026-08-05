d5f610d9cfdd7dc237746be2a4188f16c83bfd2f
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2026-08-05 20:44:07 -0300

    removed back from space invaders

--- app/[locale]/space-invaders/page.tsx
+++ app/[locale]/space-invaders/page.tsx
@@ -37,26 +37,6 @@ export default async function SpaceInvadersPage({
                 text-[#F4EEDB]
             "
         >
-            <Link
-                href={`/${locale}`}
-                className="
-                    fixed
-                    left-5
-                    top-5
-                    z-50
-                    font-mono
-                    text-xs
-                    uppercase
-                    tracking-[0.25em]
-                    text-[#F4EEDB]
-                    transition-opacity
-                    hover:opacity-60
-                    md:left-8
-                    md:top-8
-                "
-            >
-                ← {locale === 'pt' ? 'Voltar' : 'Back'}
-            </Link>
 
             <SpaceInvaders locale={locale} />
         </main>
